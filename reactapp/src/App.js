ee7677c054bab95c5eeca5f311bc43f816843f97
Iamneo <techadmin@iamneo.ai>
2023-09-27 08:30:23 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,9 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name]
   return (
     <div className="App">
-      
+      <h1>Hey!! Greeting</h1>
+      <form>
+        <label>Enter your name</label>
+        <input placeholder='User'/>
+        <button className='btn'>Say Hello</button>
+      </form>
     </div>
   );
 }
