1c4143ea1f9d5e22eb931717c82f8880b00f0f28
Iamneo <techadmin@iamneo.ai>
2023-09-27 08:35:22 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,15 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [name]
+  const [name,setName]=useState('');
+  const handleChange=(e)=>{
+       setName(e.target.value);
+  }
   return (
     <div className="App">
       <h1>Hey!! Greeting</h1>
       <form>
-        <label>Enter your name</label>
-        <input placeholder='User'/>
-        <button className='btn'>Say Hello</button>
+        <label id="data-testid">Enter your name</label>
+        <input placeholder='User' name="fname" onChange={handleChange}/>
+        <button className='btn' on>Say Hello</button>
       </form>
+      <h2>Hello, {name}!</h2>
     </div>
   );
 }
